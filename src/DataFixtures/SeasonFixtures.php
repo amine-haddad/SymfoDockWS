5c1cac47b3add43cea8b6b75326a08558662bac0
amine-haddad <seniortouco@outlook.fr>
2024-07-08 00:35:26 +0200

    mise en place de Faker creation de fausse donnée pour les categories, programs, saisons et episodes et creation de SeasonFixtures, EpisodeFixtures et ajustement de ProgramFixtures, et CategoryFixtures

--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -6,187 +6,28 @@ use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
-    const SEASONS = [
-        [
-            'number' => 1,
-            'year' => 2010,
-            'description' => "Le temps d'une saison",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 2,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 3,
-            'year' => 2010,
-            'description' => "Le temps d'une saison",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 4,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 5,
-            'year' => 2010,
-            'description' => "Le temps d'une saison",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 6,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 7,
-            'year' => 2010,
-            'description' => "Le temps d'une saison",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 8,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 9,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 10,
-            'year' => 2010,
-            'description' => "Le temps d'une saison",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 11,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'Walking Dead',
-        ],
-        [
-            'number' => 1,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'The Witcher',
-        ],
-        [
-            'number' => 2,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'The Witcher',
-        ],
-        [
-            'number' => 3,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'The Witcher',
-        ],
-        [
-            'number' => 2,
-            'year' => 2012,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'Game of Thrones',
-        ],
-        [
-            'number' => 1,
-            'year' => 2012,
-            'description' => "Le temps de 1 saison",
-            'program' => 'Friends',
-        ],
-        [
-            'number' => 4,
-            'year' => 2012,
-            'description' => "Le temps de 3 saisons",
-            'program' => 'Game of Thrones',
-        ],
-        [   
-            'number' => 2,
-            'year' => 2011,
-            'description' => "Le temps de 2 saisons",
-            'program' => 'The Witcher',
-        ],
-        [
-            'number' => 3,
-            'year' => 2012,
-            'description' => "Le temps de 3 saisons",
-            'program' => 'Game of Thrones',
-        ],
-        [
-            'number' => 4,
-            'year' => 2013,
-            'description' => "Le temps de 4 saisons",
-            'program' => 'Friends',
-        ],
-        [
-            'number' => 5,
-            'year' => 2014,
-            'description' => "Le temps de 5 saisons",
-            'program' => 'Breaking Bad',
-        ],
-        [
-            'number' => 6,
-            'year' => 2015,
-            'description' => "Le temps de 6 saisons",
-            'program' => 'Breaking Bad',
-        ],
-        [
-            'number' => 7,
-            'year' => 2016,
-            'description' => "Le temps de 7 saisons",
-            'program' => 'Breaking Bad',
-        ],
-        [
-            'number' => 8,
-            'year' => 2017,
-            'description' => "Le temps de 8 saisons",
-            'program' => 'Breaking Bad',
-        ],
-        [
-            'number' => 9,
-            'year' => 2018,
-            'description' => "Le temps de 9 saisons",
-            'program' => 'Breaking Bad',
-        ],
-        [
-            'number' => 10,
-            'year' => 2019,
-            'description' => "Le temps de 10 saisons",
-            'program' => 'Breaking Bad',
-        ],
-        [
-            'number' => 11,
-            'year' => 2021,
-            'description' => "Le temps de 11 saisons",
-            'program' => 'Breaking Bad',
-        ],
-
-    ];
-
     public function load(ObjectManager $manager)
     {
-        foreach (self::SEASONS as $key => $seasonData) {
-            $season = new Season();
-            $season->setNumber($seasonData['number']);
-            $season->setYear($seasonData['year']);
-            $season->setDescription($seasonData['description']);
-            $season->setProgram($this->getReference($seasonData['program']));
-            $manager->persist($season);
-            
-            // Définir la référence avec une clé unique
-            $this->addReference('season_' .$key , $season);
+        $faker = Factory::create('fr_FR');
+
+        for ($programKey = 1; $programKey <= 10; $programKey++) {
+            $programRef = $this->getReference('program_' . $programKey);
+            $startYear = $faker->numberBetween(1990, 2015); // Random start year between 1990 and 2015
+
+            for ($seasonKey = 1; $seasonKey <= 5; $seasonKey++) {
+                $season = new Season();
+                $season->setProgram($programRef);
+                $season->setNumber($seasonKey);
+                $season->setYear($startYear + $seasonKey - 1); // Increment the year for each season
+                $season->setDescription($faker->paragraph());
+                $manager->persist($season);
+
+                $this->addReference('season_' . $programKey . '_' . $seasonKey, $season);
+            }
         }
 
         $manager->flush();
@@ -199,3 +40,4 @@ class SeasonFixtures extends Fixture implements DependentFixtureInterface
         ];
     }
 }
+

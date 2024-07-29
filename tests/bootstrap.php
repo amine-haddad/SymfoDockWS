ee70e5f3fdf308a6d88ce40556efc4bf2426df21
amine-haddad <seniortouco@outlook.fr>
2024-07-29 19:22:44 +0200

    ajout d un fichier heroku et de test de bootstrap en realisant une bdd de test dont j ai fait une fixtures load et bien sur en creant un fichier .env.test.local avec les parametre de la bdd de test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,11 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+
+// executes the "php bin/console cache:clear" command
+passthru(sprintf(
+  'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
+  $_ENV['APP_ENV'],
+  __DIR__
+));
